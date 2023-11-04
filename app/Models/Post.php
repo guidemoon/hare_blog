ac951a66a76c971f8883bcf584a62e60b5598afc
guidemoon <mamoru.sunohara0321prv@gmail.com>
2023-11-04 15:12:48 +0900

    照会の表示完了

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,14 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 use Psy\TabCompletion\Matcher\FunctionsMatcher;
 
 class Post extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'title',
+        'body',
+    ];
+
     public Function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function getImageUrlAttribute()
+    {
+        return Storage::url('images/posts/' . $this->image);
     }
 }
