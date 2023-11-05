2e7f284b41661630f0991c64ea103d3ce1bdfa0a
guidemoon <mamoru.sunohara0321prv@gmail.com>
2023-11-05 11:30:57 +0900

    コメント登録機能作成

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use App\Models\Post;
@@ -37,5 +38,8 @@ Route::resource('posts', PostController::class)
 Route::resource('posts', PostController::class)
     ->only(['show', 'index']);
 
-require __DIR__.'/auth.php';
+Route::resource('posts.comments',CommentController::class)
+    ->only(['create', 'store', 'edit', 'update', 'destroy'])
+    ->middleware('auth');
 
+require __DIR__.'/auth.php';
